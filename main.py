e0ae7f426ddafbf59fbfdd3a4a7354b80ce6dede
adminlgmd <gogagggg5@gmail.com>
2025-01-12 16:16:45 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,64 +1,14 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
-import io
-from PyQt6 import uic
-from PyQt6.QtGui import QPainter, QColor
+from UIproject import Ui_MainWindow
 from random import randint
-
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>757</width>
-    <height>431</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="create_circle">
-    <property name="geometry">
-     <rect>
-      <x>10</x>
-      <y>20</y>
-      <width>111</width>
-      <height>23</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Нажми меня</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>757</width>
-     <height>21</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-
-"""
+from PyQt6.QtGui import QColor, QPainter
 
 
-class DrawCircle(QMainWindow):
+class DrawCircle(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.setupUi(self)
         self.do_paint = False
         self.create_circle.clicked.connect(self.paint)
 
@@ -75,7 +25,7 @@ class DrawCircle(QMainWindow):
         self.update()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 250), randint(0, 250), randint(0, 250)))
         r = randint(0, 250)
         qp.drawEllipse(150, 150, r, r)
 
